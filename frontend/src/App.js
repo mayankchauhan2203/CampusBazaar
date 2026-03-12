aebca57b42a899dd684d98a983ae8384330326c4
Mayank Chauhan <cmayank149@gmail.com>
2026-03-12 22:45:58 +0530

    Added React frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>CampusBazaar</h1>
+      <p>Buy and sell items inside IIT Delhi</p>
+    </div>
+  );
+}
+
+export default App;
